9b3554bf5809b6e2bac427c2bfa3addeeb8cf157
lieby weintraub <liebahinda@gmail.com>
2019-08-05 17:52:53 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,15 @@ class UsersController < ApplicationController
 	  @user=User.new
 	  
 	end 
-	def create 
-	 
-	end 
-	
-	end 
+	 def create
+   @user= User.new(user_params)
+   if @user 
+     
+  end
+ 
+  private
+ 
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+end
