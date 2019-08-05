2164616ad9089a91566f1c3717cb4f36a4857a62
lieby weintraub <liebahinda@gmail.com>
2019-08-05 17:54:16 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
    @user= User.new(user_params)
    if @user.save 
      session[:user_id]=@user.id 
-     
+     redirect_to user_path(@user)
+   else 
      
   end
  
