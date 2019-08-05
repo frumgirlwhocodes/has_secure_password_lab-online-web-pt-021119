958530b35102329a94f3541d3b39c38ba45f020c
lieby weintraub <liebahinda@gmail.com>
2019-08-05 17:53:45 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
 	 def create
    @user= User.new(user_params)
    if @user.save 
-     session[:user_id]
+     session[:user_id]=@user.id 
+     
      
   end
  
